45f527f6380417c94bf4ebb178691bf58cad8cc5
janfriderici <jan@friderici.net>
2022-12-01 19:13:12 +0100

    added ocaml.

--- ocaml/day1.sh
+++ ocaml/day1.sh
@@ -3,6 +3,11 @@
 #!nix-shell --pure -i ocaml -p ocaml
 *)
 
+type elf = {id : int; calories : int}
+
+let print_elf elf =
+ Format.sprintf "@[<1>%i@ :: %i@]@." elf.id elf.calories;;
+
 let read_file filename =
 let lines = ref [] in
 let chan = open_in filename in
@@ -14,11 +19,35 @@ with End_of_file ->
   close_in chan;
   List.rev !lines ;;
 
-let lines = (read_file "./inputs/day1/calories.txt");;
+let lines = (read_file "../crates/day1/calories.txt");;
 
 let parse_line line =
+try
+  int_of_string line
+with failure ->
+  -1;;
 
-let should_now_work = List.fold_left  int_of_string lines;;
+let hanle_line acc line =
+try
+  let calories = int_of_string line in
+  let an_elf = List.hd acc in
+    {id =an_elf.id; calories = calories + an_elf.calories} :: List.tl acc
+with failure ->
+  let an_elf = List.hd acc in
+    {id = an_elf.id + 1; calories = 0} :: acc;;
 
-let text = String.concat "\n" (read_file "./inputs/day1/calories.txt");;
-Format.printf "@[<1>%s@]@." text;;
+let calories_per_elf = List.fold_left hanle_line [{id = 1; calories = 0}] lines;;
+
+let masters_of_calories =
+  let sorted = List.sort
+    (fun a b -> compare b.calories a.calories)
+    calories_per_elf in
+    match sorted with
+      | [] -> "No elves to be found!"
+      | a :: [] -> print_elf a
+      | a :: b :: [] -> String.concat ", " [(print_elf a); (print_elf b)]
+      | a :: b :: c ::rest -> String.concat ", " [(print_elf a); (print_elf b); (print_elf c)];;
+
+Format.printf "@[<1>%s@]@." masters_of_calories
+
+
